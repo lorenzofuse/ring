519f02e67ea0eb465d0e01ed905767c240374165
Lorenzo <151573518+lorenzofuse@users.noreply.github.com>
2025-06-12 09:46:25 +0200

    sosa2

--- node.js
+++ node.js
@@ -1,9 +1,4 @@
-const QRCode = require('qrcode');
 
-QRCode.toFile('qr_tesi.png', 'https://github.com/lorenzofuse/ring/blob/main/ring.mp4', function (err) {
-  if (err) throw err;
-  console.log('QR code generato!');
-});
 
 const QRCode = require('qrcode');
 
